409bb8386f9c84bb9b2c6ea710668537698ea892
Lomar Lilly <lithiumlilly3@gmail.com>
2017-01-26 19:52:07 -0500

    Update array.java

--- array.java
+++ array.java
@@ -2,21 +2,45 @@ package array;
 
 import java.util.Scanner;
 
-public class driver {
+public class driver 
+{
 
-	public static void main(String[] args) {
-		int[][] arr = new int[4][3];
-		Scanner num = new Scanner(System.in);  // Reading from System.in
-	
-		
-		for(int i=0;i<=12;i++)
+	public static void main(String[] args) 
+	{
+		int[][] arr = new int[4][3];           // CREATED A 4x3 ARRAY (4 = ROW 3 = COLOUMN)
+		Scanner num = new Scanner(System.in); 
+		for(int row=0; row<4; row++)           //ROW MANIPULATION LOOP
 		{
-			for(int j=i;j<i;j++){
+			for(int col=0; col<=2; col++)      //COLOUMN MANIPULATION LOOP
+			{
 				System.out.println("Enter a number: ");
-				int n = num.nextInt();
-				arr[i][j] = n;
-				
+				arr[row][col] = num.nextInt();	//ADDS NUMBER TO ARRAY (ROW 0 COLOUMN 0 = 4)
 			}
 		}
+		
+		System.out.println("\nOUTPUT 1");
+		 for(int row=0; row<4; row++) 
+		 {
+		       for(int col=0; col<=2; col++)
+		       {
+		    	   if(row==3 && col==2){
+		    		   System.out.print(arr[row][col]);
+		    	   }
+		    	   else
+		    	   {
+		    		   System.out.print(arr[row][col] + ", ");
+		    	   }
+		       }
+		    }
+		
+		 System.out.println("\n\nOUTPUT 2");
+		 for(int row=0; row<4; row++) 
+		 {
+			 for(int col=0; col<=2; col++)
+		     {
+				 System.out.print(arr[row][col] + "\t");
+		     }
+		     System.out.println();
+		  }
 	}
 }
