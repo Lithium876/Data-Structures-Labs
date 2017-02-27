13ebc5ab7170c86ff3017eae015b3b546fb13487
Lomar Lilly <lithiumlilly3@gmail.com>
2017-02-26 22:30:38 -0500

    Update DSLabTest1.java

--- Lab_Tests/DSLabTest1.java
+++ Lab_Tests/DSLabTest1.java
@@ -1,38 +1,92 @@
-public class linear_search_recursion {
-	public static void main(String[] args) {
-		linear_search_recursion LSR = new linear_search_recursion();
-		int integer_array[] = {1,5,7,3,2,6};
-		String string_array[] = {"Paul", "James", "Bob", "Lilly", "Anna"};
-		int Index = 0;
-		
-		int int_res = LSR.IntegerLinearSearchRecursion(integer_array,integer_array.length, Index, 3);
-		int str_res = LSR.StringLinearSearchRecursion(string_array, string_array.length, Index, "Bob");
-		
-		System.out.println("Index returned for integer array search: "+int_res);
-		System.out.println("Index returned for string array search: "+str_res);
-	}
 
-	public int IntegerLinearSearchRecursion(int Int_Arr[], int Size, int Index, int SValue ){
-		if(Index==Size){
-			return -1;
+public class labTest1 {
+
+	public static void main(String[] args) {
+		labTest1 LT1 = new labTest1();
+		String fruit[] ={"apple", "mango", "pear", "pineapple", "lychee", "strawberry"};
+		System.out.print("Original List:  ");
+		for(int l=0;l<fruit.length;l++){
+			if(l==fruit.length-1){
+				System.out.print(fruit[l]);
+			}else{
+				System.out.print(fruit[l]+", ");
+			}
 		}
-		if(Int_Arr[Index] == SValue){
-			return Index;
+		System.out.println("\n");
+		
+		//Task 3
+		String sorted[] = LT1.insertionSort(fruit, fruit.length);
+		//End of Task 3
+		
+		//Task 5
+		int str_res1 = LT1.BinarySearch(sorted, sorted.length, "pineapple");
+		int str_res2 = LT1.BinarySearch(sorted, sorted.length, "grape");
+		
+		if(str_res1==-1){
+			System.out.println("\nSorry, Your search value was not found");
 		}else{
-			Index++;
-			return IntegerLinearSearchRecursion(Int_Arr, Size, Index, SValue );
+			System.out.println("\nThe index of you search value is: "+str_res1);
 		}
+		
+		if(str_res2==-1){
+			System.out.println("Sorry, Your search value was not found");
+		}else{
+			System.out.println("The index of you search value is: "+str_res2+"\n");
+		}
+		//End of Task 5
+		
+
 	}
 	
-	public int StringLinearSearchRecursion(String Str_Arr[], int Size, int Index, String SValue ){
-		if(Index==Size){
-			return -1;
+	//TASK 3 - [7 Marks]
+	public String[] insertionSort(String str_Arr[], int Size){
+		labTest1 LT1 = new labTest1();
+		int Index, Cnt2;
+		String Temp;
+		for(Index=1;Index<Size;Index++){
+			Temp = str_Arr[Index];
+			Cnt2 = Index;
+			while(Cnt2>0){
+				if(Temp.compareTo(str_Arr[Cnt2-1]) < 0){
+					break;
+				}
+				str_Arr[Cnt2] = str_Arr[Cnt2-1];
+				Cnt2--;
+			}
+			str_Arr[Cnt2] = Temp;
+			
+			System.out.print("Pass "+Index+":\t\t");
+			for(int l=0;l<Size;l++){
+				if(l==str_Arr.length-1){
+					System.out.print(str_Arr[l]);
+				}else{
+					System.out.print(str_Arr[l]+", ");
+				}
+			}
+			System.out.println();
 		}
-		if(Str_Arr[Index].equals(SValue)){
-			return Index;
-		}else{
-			Index++;
-			return StringLinearSearchRecursion(Str_Arr, Size, Index, SValue );
+		return str_Arr;
+	}
+	
+	//Task 4 - [8 Marks]
+	public int BinarySearch(String str_Arr[], int Size, String SValue){
+		int Start, End, Mid;
+		Start = 0;
+		End = Size - 1;
+		
+		while(Start <= End){
+			Mid = (Start+End)/2;
+			
+			if(SValue.equals(str_Arr[Mid])){
+				return Mid;
+			}
+			if(SValue.compareTo(str_Arr[Mid]) > 0){
+				End = Mid - 1;
+			}
+			if(SValue.compareTo(str_Arr[Mid]) < 0){
+				Start = Mid + 1;
+			}
 		}
+		return -1;
 	}
 }
